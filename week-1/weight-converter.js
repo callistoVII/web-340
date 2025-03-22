51c290add52308ae185f8e3a1d15740fe6761c5c
Nyx <xamarynth@gmail.com>
2025-03-22 19:36:58 -0400

    Week 1 Assignment + .gitignore

---
+++ week-1/weight-converter.js
@@ -0,0 +1,26 @@
+/**
+ * Author: Nicole Nielsen
+ * Date: 03/22/2025
+ * File Name: Assignment 1.3
+ * Description: Node.js Coursework, Week 1
+ */
+
+"use strict";
+
+// TO DO: Implement the weight conversion logic here
+// Write a script to convert lbs to kgs.
+
+function convertLbsToKg(weightInLbs) {
+  if (!weightInLbs[0]) {
+    console.log(`stderr: Usage: node weight-converter.js ${weightInLbs}`);
+  } else if (isNaN(weightInLbs[0])) {
+    console.log("Input must be a number.");
+  } else {
+    let weightInKg = weightInLbs[0] * 0.45359237;
+    const roundedWeightInKg = weightInKg.toFixed(2);
+    console.log(`${weightInLbs[0]} pounds is ${roundedWeightInKg} kilograms.`);
+  }
+}
+
+const args = process.argv.slice(2);
+convertLbsToKg(args);
